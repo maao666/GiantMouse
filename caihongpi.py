00e53f3ba6a98bda4b79cd650a4a688e0892fedf
Mason Ma <jiaaom@uci.edu>
2019-10-07 15:13:19 -0700

    Update

--- caihongpi.py
+++ caihongpi.py
@@ -3,6 +3,7 @@ import datetime
 import logging
 from time import sleep
 import traceback
+import os
 
 logging.basicConfig(level=logging.INFO)
 
@@ -17,10 +18,15 @@ def get_chp(api_url="https://chp.shadiao.app/api.php?from_shortcuts",
     finally:
         r.close()
 
-def write_to_csv(filename='chp_DB.csv', time_interval=0.5):
+def write_to_csv(filename='chp_DB1.csv', time_interval=0.5):
     maximum_logging_string_length = 20
+    run_times = 0
+    if not os.path.exists(filename):
+        with open(filename, 'w', encoding="utf-8") as f:
+            pass
     while True:
         try:
+            run_times = run_times + 1
             chp_text = get_chp()
             with open(filename, 'r', encoding="utf-8") as f:
                 file_content = f.read()
@@ -28,14 +34,14 @@ def write_to_csv(filename='chp_DB.csv', time_interval=0.5):
             if file_content.find(chp_text) == -1:
                 csv_line = '''"{0}","{1}"\n'''.format(
                     chp_text, datetime.datetime.now())
-                logging.info("[{}] Writing {}...".format(datetime.datetime.now(),
+                logging.info("{}[{}] Writing {}...".format(run_times, datetime.datetime.now(),
                     csv_line[: min(len(csv_line), maximum_logging_string_length)]))
-                logging.info("[{}] Total items: {}".format(
+                logging.info("{}[{}] Total items: {}".format(run_times,
                     datetime.datetime.now(), total_num + 1))
                 with open(filename, 'a', encoding="utf-8") as f:
                     f.write(csv_line)
             else:
-                logging.info("[{}] Skipping {}...".format(datetime.datetime.now(),
+                logging.info("{}[{}] Skipping {}...".format(run_times, datetime.datetime.now(),
                                                           chp_text[:min(len(chp_text), maximum_logging_string_length)]))
         except Exception:
             traceback.print_exc()
